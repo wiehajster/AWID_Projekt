e0adcfaf80240524616e7ad5b3adef5d4131d613
Natalia Strawa <natalia.strawa271@gmail.com>
2022-06-29 12:53:17 +0200

    uncomment

--- test4.sh
+++ test4.sh
@@ -6,7 +6,7 @@ echo Execute test4_ok.jl
 julia test4_ok.jl
 echo Execute test4_sk.py
 python test4_sk.py
-#echo Execute test4_cnn.py
-#python test4_cnn.py
+echo Execute test4_cnn.py
+python test4_cnn.py
 echo Execute test4_plot.py
 python test4_plot.py
